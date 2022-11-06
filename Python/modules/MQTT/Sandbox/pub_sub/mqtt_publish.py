adb93875c6dd2f840d79304b3774987d4e6e405d
pierre <pierre.housseaux54@gmail.com>
2022-11-06 15:51:41 +0100

    modification on all the added files

--- Python/modules/MQTT/Sandbox/pub_sub/mqtt_publish.py
+++ Python/modules/MQTT/Sandbox/pub_sub/mqtt_publish.py
@@ -3,8 +3,14 @@ import paho.mqtt.client as mqtt
 import random
 
 
-broker = "mqtt.eclipseprojects.io"
+
+#broker = "123a425d9b0748a39d2d27a7c2d4b7eb.s2.eu.hivemq.cloud"
+#broker_port = 8883
+
+broker = "broker.hivemq.com"
+#broker = "mqtt.eclipseprojects.io"
 broker_port = 1883
+
 
 client = mqtt.Client("Client_pub")
 
