31029cbf0a905c4405b04e2c1e8c8bf200000347
Samuel <samuel.huitelec@ipsa.fr>
2022-12-05 00:38:46 +0100

    Adding startup file

---
+++ MATLAB/Scripts/Startup_drone.m
@@ -0,0 +1,27 @@
+% Startup script for project
+
+%% Code for building Simscape custom library at startup
+
+% Change to folder with package directory
+curr_proj = simulinkproject;
+cd(curr_proj.RootFolder)
+
+% Change to root folder
+cd(curr_proj.RootFolder)
+
+% If running in a parallel pool
+% do not open model or demo script
+open_start_content = 1;
+if(~isempty(ver('parallel')))
+    if(~isempty(getCurrentTask()))
+        open_start_content = 0;
+    end
+end
+
+if(open_start_content)
+    % Parameters
+    Parameters;
+    % Open Model
+    drone_build 
+    % Open Exercises
+end
