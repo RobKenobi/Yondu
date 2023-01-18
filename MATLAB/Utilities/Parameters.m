dd373a3a5d7f0d3b7f21b1d94b4c95868edab851
Samuel <samuel.huitelec@ipsa.fr>
2023-01-18 22:59:57 +0100

    adding last files

--- MATLAB/Utilities/Parameters.m
+++ MATLAB/Utilities/Parameters.m
@@ -11,20 +11,24 @@ wallz = 2.5;        % m
 walldepth = 0.1;        % m
 
 % Drone initial position
-dronez = 0.2;       % m 
+dronez = 0.01;       % m 
 %% Drone Properties
 
 % Material used
 rho   = 1.25;            % g/cm^3 
 
 % Approximate Drone mass 
-drone_mass = 0.5;     % kg
+drone_mass = 0.805;     % kg
 
 
 %% Propeller parameters
 propeller.diameter = 0.160; % m
-propeller.Kthrust  = 0.1072; 
-propeller.Kdrag    = 0.01;
+propeller.Kthrust  = 2.98*10^(-1); 
+propeller.Kdrag    = 1.14*10^(-2);
+% propeller.diameter = 0.254; % m
+% propeller.Kthrust  = 0.1072; 
+% propeller.Kdrag    = 0.01;
+
 
 rho_air        = 1.225;  % kg/m^3
 air_temperature    = 273+25; % degK
@@ -34,22 +38,29 @@ wind_speed         = 0;      % Wind speed (m/s)
 
 % Battery Capacity
 battery_capacity = 7.6*3;
-battery_nominal_voltage = 14; % V
+battery_nominal_voltage = 12; % V
 battery.internal_R = 2e-2; % Ohm
-battery_V1 = 11.5; % V
+battery_V1 = 10; % V
 
 % Motor parameters
 motor.max_torque = 0.8;  % N*m
 motor.max_power  = 160;  % W
 motor.time_const = 0.02; % sec
 motor.efficiency = 25/30*100; 
-motor.efficiency_speed = 5000; % rpm
+motor.efficiency_speed = 500; % rpm
 motor.efficiency_torque = 0.05; % N*m
 motor.rotor_damping  = 1e-7; % N*m/(rad/s)
 
 max_power = motor.max_power;
 
-
+% % Motor parameters
+% motor.max_torque = 0.8;  % N*m
+% motor.max_power  = 160;  % W
+% motor.time_const = 0.02; % sec
+% motor.efficiency = 25/30*100; 
+% motor.efficiency_speed = 5300; % rpm
+% motor.efficiency_torque = 0.05; % N*m
+% motor.rotor_damping  = 1e-7; % N*m/(rad/s)
 %% Controller parameters battery
 
 kp_motor       = 0.00375;
@@ -60,4 +71,5 @@ filtSpd_motor    = 0.001;
 limit_motor    = 0.25;
 
 
-
+received_command = 1
+cmd_gain = 1
